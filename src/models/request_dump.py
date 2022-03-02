1cfc7fcecc82644ea385911245f5493ce34fe8d7
lenny <lennyweda@gmail.com>
2022-03-02 09:32:41 +0300

    use request dump only and added query for transaction status

--- src/models/request_dump.py
+++ src/models/request_dump.py
@@ -6,9 +6,42 @@ import uuid
 
 class RequestDump(db.Model):
     uuid_ = db.Column(UUID(as_uuid=True), unique=True, primary_key=True, default=uuid.uuid4)
-    payload = db.Column(db.JSON(), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone("Africa/Nairobi")))
+    amount = db.Column(db.Integer, nullable=False, default=0)
+    phone = db.Column(db.String(12), nullable=False, default="0")
+    shortcode = db.Column(db.String(20), nullable=False, default="0")
+    accountReference = db.Column(db.String(50), nullable=False, default="Unknown")
     service = db.Column(db.String(50), nullable=False, default= "MPESA")
+    requestStatus = db.Column(db.Boolean, nullable=False, default=False)
+    serviceStatus = db.Column(db.Boolean, nullable=False, default=False)
+    checkoutRequestId = db.Column(db.String(30), nullable=True)
+    merchantRequestId = db.Column(db.String(30), nullable=True)
+    mpesaReceiptNumber= db.Column(db.String(30), nullable=True)
+    accountBalance= db.Column(db.Integer, nullable=False, default=0)
+    updated_at = db.Column(db.String(50), nullable=True)
+    appId = db.Column(db.Integer, nullable=False, default=0)
+    errorMessage = db.Column(db.String(100), nullable=False, default= "Okay")
 
     def __repr__(self):
         return f'<request type : {self.service}, uuid: {self.uuid_}>'
+    
+    @property
+    def serialize(self):
+        return {
+            "uuid": str(self.uuid_),
+            "created at": str(self.created_at),
+            "amount": str(self.amount),
+            "phone number" : self.phone,
+            "short code number" : self.shortcode,
+            "Account Reference" : self.accountReference,
+            "service request" : self.service,
+            "request status" : "success"  if self.requestStatus == True else "Fail",
+            "service status" : "success"  if self.serviceStatus == True else "Fail",
+            "checkout request ID" : self.checkoutRequestId,
+            "merchant request ID" : self.merchantRequestId,
+            "Mpesa receipt number" : self.mpesaReceiptNumber,
+            "Account Balance" : str(self.accountBalance),
+            "updated at" : self.updated_at,
+            "application ID" : str(self.appId),
+            "Error Message" : self.errorMessage
+        }
--- src/stkPush/stkPush.py
+++ src/stkPush/stkPush.py
@@ -8,41 +8,44 @@ import pytz
 from src import ResponseDump, RequestDump, db
 import uuid
 
-stkBp = Blueprint("stkPush","stkBp", url_prefix="/stk")
+stkBp = Blueprint("stkPush", "stkBp", url_prefix="/stk")
+
 
 @stkBp.route("/", methods=["POST"])
 def make_request():
-
-    #get the request data payload:
+    # get the request data payload:
     request_data = request.get_json()
 
-    #get time:
+    # if malformed payload, return error message, else proceed:
+    if ("phone" not in request_data
+            or "amount" not in request_data
+            or "shortcode" not in request_data
+            or "AccountReference" not in request_data
+            or "ApplicationId" not in request_data):
+        return jsonify({
+            "Success": False,
+            "Message": "Malformed json payload"
+        }), 400
+        # check the app id against the registered valid ids before proceeding!!
+    # get time:
     fmt = '%Y%m%d%H%M%S'
     tz_Nairobi = pytz.timezone('Africa/Nairobi')
     time_raw = datetime.now(tz_Nairobi)
     time = time_raw.strftime(fmt)
 
-    #store the request in the requestDump database:
-    """
-    Todo: Should validate the json data to guard against SQL injection attacks 
-    """
-    r = RequestDump(
-        uuid_ = uuid.uuid4(),
-        payload = request_data,
-        created_at = time_raw,
-        service = "STK PUSH"
-    )
-
-    db.session.add(r)
-    db.session.commit()
-
-    #get password from .env:
-    mpesa_pass = str(config("MPESA_PASS_KEY"));
+    # extract the data from the request:
     str_short_code = request_data.get('shortcode')
+    Amount = int(request_data.get('amount'))
+    phoneNumber = request_data.get('phone')
+    AccountReference = str(request_data.get('AccountReference'))
+    AppId = int(request_data.get('ApplicationId'))
+
+    # get password from .env:
+    mpesa_pass = str(config("MPESA_PASS_KEY"))
     short_code = int(str_short_code)
 
-    #get the password by b64 encoding the short code + mpesa pass + time:
-    str_bytes =  str_short_code + mpesa_pass + time;
+    # get the password by b64 encoding the short code + mpesa pass + time:
+    str_bytes = str_short_code + mpesa_pass + time
     str_bytes = str_bytes.encode("ascii")
     password = base64.b64encode(str_bytes)
     password = password.decode("ascii")
@@ -52,10 +55,10 @@ def make_request():
     """ Further validation needed
         For example: what if the phone number provided is > 12 characters long?
     """
-    phoneNumber = request_data.get('phone')
-    if phoneNumber.startswith('254') == False:
+
+    if not phoneNumber.startswith('254'):
         phoneNumber = '254' + str(int(phoneNumber))
-    phoneNumber = int(phoneNumber)
+    int_phoneNumber = int(phoneNumber)
 
     # make stk push
     try:
@@ -67,49 +70,173 @@ def make_request():
         api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 
         # put access_token in request headers
-        headers = { "Authorization": f"Bearer {access_token}" ,"Content-Type": "application/json" }
-
+        headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
 
         # define request body
         data = {
             "BusinessShortCode": short_code,
             "Password": password,
-            "Timestamp": time,              # timestamp format: 20190317202903 yyyyMMhhmmss 
+            "Timestamp": time,  # timestamp format: 20190317202903 yyyyMMhhmmss
             "TransactionType": "CustomerPayBillOnline",
-            "Amount": int(request_data.get('amount')),
+            "Amount": Amount,
             "PartyA": phoneNumber,
             "PartyB": short_code,
-            "PhoneNumber": phoneNumber,
+            "PhoneNumber": int_phoneNumber,
             "CallBackURL": f"https://{str(config('MPESA_CALLBACK_URL'))}/stk/callback",
-            "AccountReference": str(request_data.get('AccountReference')),
+            "AccountReference": AccountReference,
             "TransactionDesc": "STK Payment"
         }
 
         # make request and catch response and display the error message, if any,  for debugging
-        response = requests.post(api_url,json=data,headers=headers)
+        response = requests.post(api_url, json=data, headers=headers)
         jsonResponse = json.loads(response.text)
         errorMessage = jsonResponse.get('errorMessage')
+        print("errorMessage-->")
         print(errorMessage)
 
         # check response code for errors and return response
         if response.status_code > 299:
+            # store the request and the success state False:
+            # in addition, store the error message for audit:
+            if errorMessage is not None:
+                f = RequestDump(
+                    uuid_=uuid.uuid4(),
+                    created_at=time_raw,
+                    amount=Amount,
+                    phone=phoneNumber,
+                    shortcode=str_short_code,
+                    accountReference=AccountReference,
+                    service="STK PUSH",
+                    requestStatus=False,
+                    errorMessage=errorMessage,
+                    appId=AppId,
+
+                )
+                db.session.add(f)
+                db.session.commit()
+
             return jsonify({
                 "success": False,
                 "message": errorMessage
-            }),400
+            }), 400
 
-        #return the successful response to the user:
+        # return the successful response to the user and store the success response:
+        response_dict = json.loads(response.text)
+        s = RequestDump(
+            uuid_=uuid.uuid4(),
+            created_at=time_raw,
+            amount=Amount,
+            phone=phoneNumber,
+            shortcode=str_short_code,
+            accountReference=AccountReference,
+            service="STK PUSH",
+            requestStatus=True,
+            checkoutRequestId=response_dict.get('CheckoutRequestID'),
+            merchantRequestId=response_dict.get('MerchantRequestID'),
+            appId=AppId,
+
+        )
+        db.session.add(s)
+        db.session.commit()
+
         return jsonify({
             "Response": json.loads(response.text)
-        }),200
+        }), 200
 
-    except:
-
+    except Exception as e:
+        print(e)
         # catch error and return responses:
         return jsonify({
-            "success":False,
-            "message":"Sorry, an unexpected exception occurred. Please contact Admin."
-        }),400
+            "success": False,
+            "message": "Sorry, an unexpected exception occurred. Please contact Admin."
+        }), 400
+
+
+"""This is the callback url:"""
+
+@stkBp.route("/callback", methods=["POST"])
+def callbackResponse():
+    callback_response = request.get_json()
+    print(callback_response)
+    print("Now save the response!")
+    tz_Nairobi = pytz.timezone('Africa/Nairobi')
+    time_raw = datetime.now(tz_Nairobi)
+    # store the response:
+    resp = ResponseDump(
+        uuid_=uuid.uuid4(),
+        payload=callback_response,
+        created_at=time_raw,
+        service="STK PUSH"
+    )
+
+    db.session.add(resp)
+    db.session.commit()
+
+    # send the response to the callback url:
+
+    return jsonify("Response registered!!"), 200
+
+@stkBp.route("/fetchAllTransactions", methods = ["GET"])
+def fetchAllTransactions():
+    t = RequestDump.query.all()
+    transactions = [data.serialize for data in t]
+    return jsonify(transactions), 200
+
+
+""" Check the status of the request from Safaricom """
+@stkBp.route("/checkStatus", methods = ["POST"])
+def checkTransactionStatus():
+    request_data = request.get_json()
+
+    #first validate the payload before sending the request:
+
+    CheckoutRequestID = request_data.get('CheckoutRequestID')
+    str_short_code = request_data.get('shortcode')
+
+    #calculate the timestamp:
+    fmt = '%Y%m%d%H%M%S'
+    tz_Nairobi = pytz.timezone('Africa/Nairobi')
+    time_raw = datetime.now(tz_Nairobi)
+    time = time_raw.strftime(fmt)
+
+    # get password from .env:
+    mpesa_pass = str(config("MPESA_PASS_KEY"))
+    short_code = int(str_short_code)
+
+    # get the password by b64 encoding the short code + mpesa pass + time:
+    str_bytes = str_short_code + mpesa_pass + time
+    str_bytes = str_bytes.encode("ascii")
+    password = base64.b64encode(str_bytes)
+    password = password.decode("ascii")
+    password = str(password)
+
+     # get access_token
+    access_token = get_mpesa_token()
+
+    # stk_push request url
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+
+    # put access_token in request headers
+    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+
+    #data:
+    data = {
+        "BusinessShortCode" : short_code,
+        "Password": password,
+        "Timestamp": time,
+        "CheckoutRequestID" : CheckoutRequestID
+        
+    }
+
+    #response:
+    response = requests.post(api_url, json=data, headers=headers)
+    jsonResponse = json.loads(response.text)
+    
+    return jsonify({
+        "Response":jsonResponse
+    }), 200
+
+
 
 # get Authentication token from M-pesa [function]:
 def get_mpesa_token():
@@ -122,26 +249,3 @@ def get_mpesa_token():
 
     # return access_token from response
     return r.json().get('access_token')
-    
-"""This is the callback url:"""
-@stkBp.route("/callback", methods=["POST"])
-def callbackResponse():
-    callback_response = request.get_json()
-    print(callback_response)
-    tz_Nairobi = pytz.timezone('Africa/Nairobi')
-    time_raw = datetime.now(tz_Nairobi)
-    #store the response:
-    resp = ResponseDump(
-        uuid_ = uuid.uuid4(),
-        payload = callback_response,
-        created_at = time_raw,
-        service = "STK PUSH"
-    )
-
-    db.session.add(resp)
-    db.session.commit()
-    
-    #send the response to the callback url:
-
-
-    return jsonify("Response registered!!"), 200
