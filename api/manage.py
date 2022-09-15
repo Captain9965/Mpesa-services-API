ff8befc9690aca32e64b3af78dd82f7054c542e9
lenny <lennyweda@gmail.com>
2022-09-15 14:00:54 +0300

    (release/v1.0.0): use FlaskGroup manage.py

--- api/manage.py
+++ api/manage.py
@@ -1,12 +1,15 @@
 from src import create_app
-from decouple import config
+from flask.cli import FlaskGroup
 import os
 
+
 new_app = create_app()
+cli = FlaskGroup(new_app)
 new_app.route("/")
 def home():
     return "<body>Welcome!</body>"
 
 if __name__ == "__main__":
     os.environ['AUTHLIB_INSECURE_TRANSPORT'] = '1'
-    new_app.run(host= config('APPLICATION_HOST'))
+    cli()
+    
